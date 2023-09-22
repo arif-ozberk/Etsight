12dda4034dc5070fd533c7a5ad2e431ad2571fc7
arif-ozberk <ozberkazmak@gmail.com>
2023-09-22 13:46:14 +0300

    setup tailwind & sass

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
+// Styles
+import styles from "../styles/HomePage.module.scss";
+
 
 const HomePage = () => {
     return (
-        <div>
-            HomePage
+        <div className={`${styles.container}`}>
+            <h1 className={`${styles.heading} text-2xl text-red-500`}>Home Page</h1>
+            <p className='text-emerald-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, asperiores?</p>
         </div>
     );
 }
