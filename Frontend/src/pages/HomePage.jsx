382608d961c62806dc1af9397b9e01e4cbc2e6e0
arif-ozberk <ozberkazmak@gmail.com>
2023-09-24 13:29:08 +0300

    setup responsive breakpoints

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ import styles from "../styles/HomePage.module.scss";
 const HomePage = () => {
     return (
         <div className={`${styles.container}`}>
-            <h1 className={`${styles.heading} text-2xl text-red-500`}>Home Page</h1>
+            <h1 className={`${styles.heading} text-7xl 2xl:text-5xl xl:text-3xl lg:text-xl md:text-lg sm:text-base xs:text-sm text-red-500`}>Home Page</h1>
             <p className='text-emerald-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, asperiores?</p>
         </div>
     );
